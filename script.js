ac625e2b18b162cffc6057419f78d134a342b9d5
Siarhei Prymakovich <prima.psa@gmail.com>
2019-11-06 18:01:45 +0300

    feat: draw keyboard from conf

---
+++ script.js
@@ -0,0 +1,18 @@
+document.body.insertAdjacentHTML("afterbegin",'<div class="wrapper" id="wrapper"><textarea  class="area" id="result" rows="5" cols="30"></textarea><div class="keyboard" id="keyboard"></div>');
+     for (let i = 0; i < config.length; i++){
+      let divLine = document.createElement('div');
+      divLine.classList.add('line');
+      for(let j = 0; j < config[i].length; j++){
+        let divKey = document.createElement('div');      
+        divKey.classList.add('key');   
+        divKey.classList.add(`key-${config[i][j]['key']}`);    
+        divKey.insertAdjacentHTML('afterbegin',`<div class="${config[i][j].key} show">
+        <span class="en lower active">${config[i][j]['en lower']}</span>
+        <span class="en upper disabled">${config[i][j]['en upper']}</span></div>
+        <div class="${config[i][j].key} disabled"><span class="ru lower active">${config[i][j]['ru lower']}</span>
+        <span class="ru upper disabled">${config[i][j]['ru upper']}</span></div>`);
+        divLine.append(divKey);
+      }   
+      keyboard.append(divLine);   
+  }
+wrapper.append(keyboard);
